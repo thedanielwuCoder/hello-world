f313af8e428d86f91162c8dfe9899c90c6cfe60a
thedanielwuCoder <159278599+thedanielwuCoder@users.noreply.github.com>
2025-11-12 15:36:51 -0500

    Delete Database directory

--- Database/03_cleanup_drop.sql
+++
@@ -1,9 +0,0 @@
-USE user_management;
-
--- Drop tables
-DROP TABLE IF EXISTS CIC_CourseOffering;
-DROP TABLE IF EXISTS CIC_FacultyPreference;
-DROP TABLE IF EXISTS CIC_Program;
-DROP TABLE IF EXISTS CIC_Semester;
-DROP TABLE IF EXISTS CIC_Course;
-DROP TABLE IF EXISTS CIC_FacultyProfile;
